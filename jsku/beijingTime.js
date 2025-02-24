2b85ed3415cb752d15764f12860f2e92d205434c
yutian81 <yutian88881@hotmail.com>
2025-02-24 19:04:08 +0800

    Update beijingTime.js

--- jsku/beijingTime.js
+++ jsku/beijingTime.js
@@ -1,3 +1,4 @@
+// 获取北京时间的 Date 对象
 function getBeijingDate() {
     const formatter = new Intl.DateTimeFormat('zh-CN', {
         timeZone: 'Asia/Shanghai',
@@ -13,12 +14,14 @@ function getBeijingDate() {
     const { year, month, day, hour, minute, second } = Object.fromEntries(
         parts.map(({ type, value }) => [type, value])
     );
-    // 构建一个表示北京时间的 Date 对象
-    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
+    // 使用 Date.UTC 创建 UTC 时间的时间戳
+    const utcTime = Date.UTC(year, month - 1, day, hour, minute, second);
+    return new Date(utcTime);
 }
 
+// 获取北京时间的字符串
 function getBeijingStr() {
-    return new Intl.DateTimeFormat('zh-CN', {
+    const formatter = new Intl.DateTimeFormat('zh-CN', {
         timeZone: 'Asia/Shanghai',
         year: 'numeric',
         month: '2-digit',
