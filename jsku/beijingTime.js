26328be9e792ba1f5ca6127e84d305066eb56318
yutian81 <yutian88881@hotmail.com>
2025-02-24 18:58:40 +0800

    Update beijingTime.js

--- jsku/beijingTime.js
+++ jsku/beijingTime.js
@@ -1,19 +1,5 @@
 function getBeijingDate() {
     const formatter = new Intl.DateTimeFormat('zh-CN', {
-        timeZone: 'Asia/Shanghai', hour12: false,
-        year: 'numeric', month: '2-digit', day: '2-digit',
-        hour: '2-digit', minute: '2-digit', second: '2-digit'
-    });
-    const parts = formatter.formatToParts(new Date());
-    const { year, month, day, hour, minute, second } = Object.fromEntries(
-        parts.map(({ type, value }) => [type, value])
-    );
-    // 构建一个表示北京时间的 Date 对象
-    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
-}
-
-function getBeijingStr() {
-    const formatter = new Intl.DateTimeFormat('zh-CN', {
         timeZone: 'Asia/Shanghai',
         year: 'numeric',
         month: '2-digit',
@@ -27,6 +13,19 @@ function getBeijingStr() {
     const { year, month, day, hour, minute, second } = Object.fromEntries(
         parts.map(({ type, value }) => [type, value])
     );
-    // 构建一个表示北京时间的字符串
-    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+    // 构建一个表示北京时间的 Date 对象
+    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
+}
+
+function getBeijingStr() {
+    return new Intl.DateTimeFormat('zh-CN', {
+        timeZone: 'Asia/Shanghai',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    }).format(new Date()).replace(/\//g, '-');
 }
